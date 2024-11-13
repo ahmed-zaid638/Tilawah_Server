95025f3263e1c2baeb08d8deebf66f465da2dfa3
mohammadJBYaseen <mohammad.yaseen1988@gmail.com>
2024-11-13 20:17:38 +0100

    Entity for role permissions, fix db config, do schema validation.

--- src/models/index.ts
+++ src/models/index.ts
@@ -4,7 +4,6 @@ import config from '../config/config';
 
 const env = process.env.NODE_ENV || 'development';
 const dbConfig = (config as any)[env];
-console.log(JSON.stringify(dbConfig, null,));
 const sequelize = new Sequelize({
   ...dbConfig,
   models: [path.join(__dirname, '/*.model.ts')], // Automatically loads all models
